36c094e5bdc1939617c142279e2fcd2b2cd6d3fc
benjimon24 <benji102491@gmail.com>
2018-02-08 22:37:04 -0500

    write temporary code for GET request to third-party API to test functionality. write query to find films under the same genre

--- index.js
+++ index.js
@@ -22,27 +22,52 @@ Promise.resolve()
       release_date: Sequelize.STRING,
 			tagline: Sequelize.STRING,
 			genre_id: Sequelize.INTEGER,
-		}
+		}, {
+      timestamps: false
+    }
 	);
 
 // ROUTES
-app.get('/', helloWorld);
+// app.get('/', helloWorld);
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
-function helloWorld(req, res) {
-  Film.findOne({
-    attributes: ['title', 'release_date', 'tagline'],
-    where: {
-      title : 'Cyborg Sobek Statistician',
-    }
-  }).then(function(response) {
-      res.json(response);
-    })
-}
+// function helloWorld(req, res) {
+//   Film.findOne({
+//     attributes: ['title', 'release_date', 'tagline'],
+//     where: {
+//       title : 'Cyborg Sobek Statistician',
+//     }
+//   }).then( (response) => {
+//       res.json(response);
+//     })
+// }
+
 
 function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
+  Film.findById(req.params.id).then( (paramsFilm) => {
+    if (paramsFilm == null) {
+      res.status(500).send("Cannot find a film matching this ID.")
+    }
+    else {
+      Film.findAll({
+        where: {
+          genre_id: paramsFilm.genre_id
+        }
+      }).then( (filmRecommendations) => {
+        res.json(filmRecommendations)
+      })
+    }
+
+  })
+
+  // request.get(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, (error, response, body) => {
+  //   // console.log(req.params)
+  //   // console.log('error:', error); // Print the error if one occurred
+  //   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //   // console.log('body:', body); // Print the HTML for the Google homepage.
+  //   res.json(body)
+  // });
 }
 
 module.exports = app;
