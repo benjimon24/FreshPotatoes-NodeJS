5e03eb0cdd5793ff9b76c7b79fffd77f518d0b0e
benjimon24 <benji102491@gmail.com>
2018-02-09 23:25:09 -0500

    parse response from third party to json and get the number of reviews

--- index.js
+++ index.js
@@ -28,30 +28,19 @@ Promise.resolve()
 	);
 
 // ROUTES
-// app.get('/', helloWorld);
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
-// ROUTE HANDLER
-// function helloWorld(req, res) {
-//   Film.findOne({
-//     attributes: ['title', 'release_date', 'tagline'],
-//     where: {
-//       title : 'Cyborg Sobek Statistician',
-//     }
-//   }).then( (response) => {
-//       res.json(response);
-//     })
-// }
-
-
 function getFilmRecommendations(req, res) {
-  Film.findById(req.params.id).then( (paramsFilm) => {
-    if (paramsFilm == null) {
+  // find parent film by id from parameters
+  Film.findById(req.params.id).then( (parentFilm) => {
+    if (parentFilm == null) {
+      // cannot find parent film
       res.status(500).send("Cannot find a film matching this ID.")
     }
     else {
-      let upperRelease = new Date(paramsFilm.release_date)
-      let lowerRelease = new Date(paramsFilm.release_date)
+      // find all films matching genre and within +/- 15 years of release date
+      let upperRelease = new Date(parentFilm.release_date)
+      let lowerRelease = new Date(parentFilm.release_date)
       upperRelease.setFullYear(upperRelease.getFullYear() + 15)
       lowerRelease.setFullYear(lowerRelease.getFullYear() - 15)
 
@@ -61,7 +50,7 @@ function getFilmRecommendations(req, res) {
       Film.findAll({
         order: [ ['id', 'ASC'] ],
         where: {
-          genre_id: paramsFilm.genre_id,
+          genre_id: parentFilm.genre_id,
           release_date: {
             $and: {
               $gt: lowerRelease,
@@ -71,24 +60,33 @@ function getFilmRecommendations(req, res) {
         }
       }).then( (filmRecs) => {
         if (filmRecs == null) {
+          // no matching films with genre and close release date
           res.status(500).send("We have no recommendations for this film.")
         }
         else {
-          // let filteredRecs = filmRecs
-          res.json(filmRecs)
+          // for each film, we need to see if they have atleast 5 reviews with the average rating being over 4.0
+          request.get(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, (error, response, body) => {
+            // console.log(req.params)
+            // console.log('error:', error); // Print the error if one occurred
+            // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            // console.log('body:', body); // Print the HTML for the Google homepage.
+            
+            reviews = JSON.parse(body)[0].reviews
+            if (reviews == []) {
+              res.status(500).send("We cannot find any reviews for this film")
+            } else {
+              console.log(filmRecs.length)
+              console.log(reviews.length)
+              res.json(reviews)
+              // let filteredRecs = filmRecs
+            }
+          });
         }
       })
     }
 
   })
 
-  // request.get(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, (error, response, body) => {
-  //   // console.log(req.params)
-  //   // console.log('error:', error); // Print the error if one occurred
-  //   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //   // console.log('body:', body); // Print the HTML for the Google homepage.
-  //   res.json(body)
-  // });
 }
 
 module.exports = app;
