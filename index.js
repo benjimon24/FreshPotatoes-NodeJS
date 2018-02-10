a55bfbee0303906e3f3c05ed9ccdffc4fd8e982b
benjimon24 <benji102491@gmail.com>
2018-02-10 02:14:52 -0500

    split getFilmRecommendations into seperate helper functions

--- index.js
+++ index.js
@@ -30,6 +30,48 @@ Promise.resolve()
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
+function isHighlyRated(filmID, callback){
+  // for each film, we need to see if they have atleast 5 reviews with the average rating being over 4.0
+
+  request.get(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${filmID}`)
+  // request.get(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${filmID}`, (error, response, body) => {
+    // let reviews = JSON.parse(body)[0].reviews.map( (review) =>  parseInt(review.rating) )
+    // let averageRating = reviews.reduce( (sum, rating) => sum + rating) / reviews.length
+    // let highlyRated;
+    // if (reviews.length > 5 && averageRating > 4) {
+    //   highlyRated = true
+    // } else {
+    //   highlyRated = false
+    // }
+    // callback(highlyRated);
+    // callback(averageRating)
+  // })
+}
+
+function findRelatedFilms(parent){
+  // find all films matching genre and within +/- 15 years of release date, and sort them by ID
+  let upperRelease = new Date(parent.release_date)
+  let lowerRelease = new Date(parent.release_date)
+  upperRelease.setFullYear(upperRelease.getFullYear() + 15)
+  lowerRelease.setFullYear(lowerRelease.getFullYear() - 15)
+
+  return Film.findAll({
+    order: [ ['id', 'ASC'] ],
+    where: {
+      genre_id: parent.genre_id,
+      release_date: {
+        $and: {
+          $gt: lowerRelease,
+          $lt: upperRelease
+        }
+      }
+    }
+  })
+}
+
+
+
+
 function getFilmRecommendations(req, res) {
   // find parent film by id from parameters
   Film.findById(req.params.id).then( (parentFilm) => {
@@ -38,49 +80,15 @@ function getFilmRecommendations(req, res) {
       res.status(500).send("Cannot find a film matching this ID.")
     }
     else {
-      // find all films matching genre and within +/- 15 years of release date
-      let upperRelease = new Date(parentFilm.release_date)
-      let lowerRelease = new Date(parentFilm.release_date)
-      upperRelease.setFullYear(upperRelease.getFullYear() + 15)
-      lowerRelease.setFullYear(lowerRelease.getFullYear() - 15)
-
-      console.log(upperRelease)
-      console.log(lowerRelease)
-
-      Film.findAll({
-        order: [ ['id', 'ASC'] ],
-        where: {
-          genre_id: parentFilm.genre_id,
-          release_date: {
-            $and: {
-              $gt: lowerRelease,
-              $lt: upperRelease
-            }
-          }
-        }
-      }).then( (filmRecs) => {
-        if (filmRecs == null) {
+      findRelatedFilms(parentFilm).then( (filmRecommendations) => {
+        if (filmRecommendations == null) {
           // no matching films with genre and close release date
           res.status(500).send("We have no recommendations for this film.")
         }
         else {
-          // for each film, we need to see if they have atleast 5 reviews with the average rating being over 4.0
-          request.get(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, (error, response, body) => {
-            // console.log(req.params)
-            // console.log('error:', error); // Print the error if one occurred
-            // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            // console.log('body:', body); // Print the HTML for the Google homepage.
-            
-            reviews = JSON.parse(body)[0].reviews
-            if (reviews == []) {
-              res.status(500).send("We cannot find any reviews for this film")
-            } else {
-              console.log(filmRecs.length)
-              console.log(reviews.length)
-              res.json(reviews)
-              // let filteredRecs = filmRecs
-            }
-          });
+          // isHighlyRated(req.params.id, (averageRating) => {test = averageRating })
+          // filmRecommendations = filmRecommendations.filter((film) => {})
+          res.json(filmRecommendations)
         }
       })
     }
