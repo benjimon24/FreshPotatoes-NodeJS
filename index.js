868ee89739f682079e0f18ab3efc895eac7f347b
benjimon24 <benji102491@gmail.com>
2018-02-12 20:39:22 -0500

    update release_date column in ORM as 'releaseDate'

--- index.js
+++ index.js
@@ -21,40 +21,58 @@ Promise.resolve()
   var Film = sequelize.define('films',
 		{
 			title: Sequelize.STRING,
-      release_date: Sequelize.DATE,
-			tagline: Sequelize.STRING,
+      releaseDate: {
+        type: Sequelize.DATE,
+        field: 'release_date',
+      },
 			genre_id: Sequelize.INTEGER,
 		}, {
       timestamps: false
     }
 	);
 
+  var Genre = sequelize.define('genres',
+  {
+    name: Sequelize.STRING,
+  }, {
+    timestamps: false
+  })
+
+  Film
+
 // ROUTES
+app.get('/films/:id', getFilm);
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
-function filterByReviews(res, films){
-  let filmIDs = relatedFilms.map( film => film.id).join(',')
-  request.get( THIRD_PARTY_API + filmIDs, (error, response, body) => {
-    reviews = JSON.parse(body)
-    reviews = reviews.filter (review => {
-      return review.reviews.length >= 5
-    })
-    reviews = reviews.map ( filmReviews => {
-            let ratings = filmReviews.reviews.map( review => review.rating)
-            filmReviews.averageRating = ratings.reduce( (sum, rating) => sum + rating) / ratings.length
-            return filmReviews
-    }).filter ( filmReviews => {
-      return filmReviews.averageRating > 4
-    })
-    res.json(reviews)
+function getFilm(req, res){
+  Film.findById(req.params.id).then( (film) => {
+    res.json(film)
   })
 }
 
+function getFilmRecommendations(req, res) {
+  let filmRecommendations = {
+    recommendations: [],
+    meta: {
+      offset: 0,
+      limit: 10
+    },
+  };
+
+  Film.findById(req.params.id).then( (parentFilm) => {
+      findRelatedFilms(parentFilm).then( (relatedFilms) => {
+        filterByReviews(relatedFilms, (recommendedFilms) => {
+          filmRecommendations.recommendations = recommendedFilms
+          res.json(filmRecommendations)
+        })
+      })
+  })
+}
 
 function findRelatedFilms(parent){
   // find all films matching genre and within +/- 15 years of release date, and sort them by ID
-  let upperRelease = new Date(parent.release_date)
-  let lowerRelease = new Date(parent.release_date)
+  let upperRelease = new Date(parent.releaseDate)
+  let lowerRelease = new Date(parent.releaseDate)
   upperRelease.setFullYear(upperRelease.getFullYear() + 15)
   lowerRelease.setFullYear(lowerRelease.getFullYear() - 15)
 
@@ -68,21 +86,30 @@ function findRelatedFilms(parent){
           $lt: upperRelease
         }
       }
-    }
-  }, raw: true)
+    }, raw: true
+  })
 }
 
+function filterByReviews(films, callback){
+  let filmIDs = films.map( film => film.id).join(',')
+  request.get( THIRD_PARTY_API + filmIDs, (error, response, body) => {
+    reviews = JSON.parse(body)
 
-
-
-function getFilmRecommendations(req, res) {
-  Film.findById(req.params.id).then( (parentFilm) => {
-      findRelatedFilms(parentFilm).then( (relatedFilms) => {
-        filterByReviews(res, relatedFilms)
-      })
-
+    let recommendedFilms = films.map( film => {
+      film.reviews = reviews.find( review => { return review.film_id == film.id }).reviews
+      return film
+    }).filter (film => {
+      return film.reviews.length >= 5
+    }).map ( film => {
+      let ratings = film.reviews.map ( review => review.rating )
+      film.averageRating = Math.round((ratings.reduce( (sum, rating) => sum + rating) / ratings.length) * 100) / 100
+      film.reviews = film.reviews.length
+      return film
+    }).filter ( film => {
+      return film.averageRating > 4
+    })
+    callback(recommendedFilms)
   })
-
 }
 
 module.exports = app;
